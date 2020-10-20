c4964bac1cb4a9ba062e9d77abbfdedae58d7d51
Felipe Lincoln <felipelincoln@usp.br>
2020-10-20 18:16:13 -0300

    replace date to datetime

--- priv/repo/migrations/20201020174000_create_events.exs
+++ priv/repo/migrations/20201020174000_create_events.exs
@@ -4,8 +4,8 @@ defmodule Calendar7.Repo.Migrations.CreateEvents do
   def change do
     create table(:events) do
       add :description, :string
-      add :starts_at, :date
-      add :ends_at, :date
+      add :starts_at, :utc_datetime
+      add :ends_at, :utc_datetime
 
       timestamps()
     end
