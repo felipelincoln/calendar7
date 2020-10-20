97e9ca391bed7372841fd099907af9028a55776e
Felipe Lincoln <felipelincoln@usp.br>
2020-10-20 14:35:57 -0300

    fix database name in test

--- config/test.exs
+++ config/test.exs
@@ -9,7 +9,7 @@ config :calendar7, Calendar7.Repo,
   username: "postgres",
   password: "postgres",
   database: "calendar7_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
+  hostname: "db",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
