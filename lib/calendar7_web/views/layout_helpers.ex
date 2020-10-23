6455ffa8201a99cc56ade94d4ec40862cf9c57f8
Felipe Lincoln <felipelincoln@usp.br>
2020-10-23 17:20:02 -0300

    fix pow bug

--- lib/calendar7_web/views/layout_helpers.ex
+++ lib/calendar7_web/views/layout_helpers.ex
@@ -4,6 +4,8 @@ defmodule Calendar7Web.LayoutHelpers do
   alias Calendar7.Repo
   alias Calendar7.Users.User
 
+  def get_user(%User{id: id}), do: get_user(id)
   def get_user(id), do: Repo.get(User, id)
+  def get_user(%User{id: id}, field), do: get_user(id, field)
   def get_user(id, field), do: get_user(id) |> Map.get(field)
 end
