5b8446c02830b3bd3b5c76f0daf7e6ee67e505aa
Felipe Lincoln <felipelincoln@usp.br>
2020-10-24 16:00:27 -0300

    add events field to user

--- lib/calendar7/users/user.ex
+++ lib/calendar7/users/user.ex
@@ -2,9 +2,13 @@ defmodule Calendar7.Users.User do
   use Ecto.Schema
   use Pow.Ecto.Schema
 
+  alias Calendar7.Manage.Event
+
   schema "users" do
     pow_user_fields()
 
     timestamps()
+
+    has_many :events, Event
   end
 end
